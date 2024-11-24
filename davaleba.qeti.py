e136ba485febd77142fc7056961e75c32eaf70d1
qetishonia1 <qeti.shonia66@gmail.com>
2024-11-24 23:19:43 +0400

    Add files via upload

---
+++ davaleba.qeti.py
@@ -0,0 +1,47 @@
+# 1
+# from math import*
+# print(round(3.23 ,1))
+# print(ceil(3.23))
+# print(floor(3.23))
+# print(trunc(3.23))
+
+# 2
+# from math import*
+# print(max(12,4,53))
+
+#3
+# import math 
+# x=int(input("Enter the number: "))
+# result=math.sqrt(x)
+# print(result)
+
+# from math import sqrt
+# x=int(input("enter the number: "))
+# result= sqrt(x)
+# print(result)
+
+# from math import*
+# x=int(input("Enter the number: "))
+# result=sqrt(x)
+# print(result)
+
+# 4
+# (a)
+# from math import*
+# x=int(input("enter the number: "))
+# result=sqrt(x)
+# print(result)
+# (b)
+# from math import*
+# x=int(input(4225))
+# result=sqrt(x)
+# print(result)
+
+# 5
+# import random
+# x=int(input("enter numbers: "))
+# random.randint(x)
+# print(random)
+
+# from math import*
+# print(round(5.34,1))
